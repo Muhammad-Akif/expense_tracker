0a1fd195928a3540c0505de3765c3abd802dc1cf
Muhammad Akif <Muhammad-Akif@outlook.com>
2021-06-12 21:59:19 +0500

    Expense Tracker App

---
+++ deployment/public/precache-manifest.d0cc8f74a62914de066942e3fb96c7f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21957030a1181be49e112bbadc1fa49c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e82e68a7e63d9034d6a",
+    "url": "/static/css/main.bf088707.chunk.css"
+  },
+  {
+    "revision": "5ae105fdb292a1efe05d",
+    "url": "/static/js/2.de407467.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de407467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e82e68a7e63d9034d6a",
+    "url": "/static/js/main.0355aa71.chunk.js"
+  },
+  {
+    "revision": "7f12465123b4ddf44410",
+    "url": "/static/js/runtime-main.811fa6da.js"
+  }
+]);
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <h2 style={{fontFamily: "Arial"}}>
+            Expense Tracker by Muhammad Akif
+        </h2>
+    )
+}
